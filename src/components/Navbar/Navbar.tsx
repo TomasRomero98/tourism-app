d5db77a9703130ad8657e631fec3a29d2579aa81
Tourism App <tomas.romer98@gmail.com>
2025-02-17 06:44:03 +0000

    fix build

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export const Navbar = () => {
   return (
     <nav className="navbar" data-testid="navbar">
       <h1>Navbar</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/dashboard">Dashboard</a>
+        <Link href="/">Home</Link>
+        <Link href="/dashboard">Dashboard</Link>
       </div>
     </nav>
   );
